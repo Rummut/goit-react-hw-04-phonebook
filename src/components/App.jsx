d686049b4601072613a88026b5457bbe0bf046af
Rummut <kzantip@gmail.com>
2023-08-29 13:08:25 +0300

    add edits

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,87 +1,63 @@
-// import { Component } from 'react';
-// import { ContactList } from './contact-list/ContactList';
-// import { ContactForm } from './contact-form/ContactForm';
-// import { Filters } from './filters/Flters';
-// import { Container, Title, TitleContact } from './App.styled';
-// import { GlobalStyle } from 'GlobalStyle';
+import { useEffect, useState } from 'react';
+import { ContactList } from './contact-list/ContactList';
+import { ContactForm } from './contact-form/ContactForm';
+import { Filters } from './filters/Flters';
+import { Container, Title, TitleContact } from './App.styled';
+import { GlobalStyle } from 'GlobalStyle';
 
-// export class App extends Component {
-//   state = {
-//     contacts: [
-//       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-//       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-//       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-//       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-//     ],
-//     filter: '',
-//   };
+const initialState = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
 
-//   componentDidMount = () => {
-//     const savedContacts = localStorage.getItem('array-contacts');
-//     if (savedContacts !== null) {
-//       this.setState({
-//         contacts: JSON.parse(savedContacts),
-//       });
-//     }
-//   };
+export const App = () => {
+  const [contacts, setContacts] = useState(() => {
+    return JSON.parse(localStorage.getItem('array-contacts')) ?? initialState;
+  });
+  const [filter, setFilter] = useState('');
+  useEffect(() => {
+    localStorage.setItem('array-contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
-//   componentDidUpdate = (prevProps, prevState) => {
-//     if (prevState.contacts !== this.state.contacts) {
-//       localStorage.setItem(
-//         'array-contacts',
-//         JSON.stringify(this.state.contacts)
-//       );
-//     }
-//   };
+  const handleContactsAdd = newContact => {
+    const { name } = newContact;
+    const isNameContain = contacts.find(
+      contact => contact.name.toLowerCase() === name.toLowerCase()
+    );
 
-//   handleContactsAdd = newContact => {
-//     const { contacts } = this.state;
-//     const { name } = newContact;
-//     const isNameContain = contacts.find(
-//       contact => contact.name.toLowerCase() === name.toLowerCase()
-//     );
+    if (isNameContain) {
+      alert(`${name} is already in contacts.`);
+      return;
+    }
+    setContacts(prevState => [...contacts, newContact]);
+  };
 
-//     if (isNameContain) {
-//       alert(`${name} is already in contacts.`);
-//       return;
-//     }
-//     this.setState(({ contacts }) => ({ contacts: [...contacts, newContact] }));
-//   };
+  const handleDelete = listDeleteId => {
+    setContacts(prevState =>
+      prevState.filter(contact => contact.id !== listDeleteId)
+    );
+  };
 
-//   handleDelete = listDeleteId => {
-//     this.setState(prevState => {
-//       return {
-//         contacts: prevState.contacts.filter(
-//           contact => contact.id !== listDeleteId
-//         ),
-//       };
-//     });
-//   };
+  const changeFilter = newFilter => {
+    setFilter(newFilter);
+  };
+  const visibleContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
 
-//   changeFilter = newFilter => {
-//     this.setState({
-//       filter: newFilter,
-//     });
-//   };
-
-//   render() {
-//     const { contacts, filter } = this.state;
-
-//     const visibleContacts = contacts.filter(contact =>
-//       contact.name.toLowerCase().includes(filter.toLowerCase())
-//     );
-//     return (
-//       <Container>
-//         <Title>Phonebook</Title>
-//         <ContactForm AddContact={this.handleContactsAdd}></ContactForm>
-//         <TitleContact>Contacts</TitleContact>
-//         <Filters value={filter} onChange={this.changeFilter}></Filters>
-//         <ContactList
-//           contacts={visibleContacts}
-//           onDelete={this.handleDelete}
-//         ></ContactList>
-//         <GlobalStyle />
-//       </Container>
-//     );
-//   }
-// }
+  return (
+    <Container>
+      <Title>Phonebook</Title>
+      <ContactForm AddContact={handleContactsAdd}></ContactForm>
+      <TitleContact>Contacts</TitleContact>
+      <Filters value={filter} onChange={changeFilter}></Filters>
+      <ContactList
+        contacts={visibleContacts}
+        onDelete={handleDelete}
+      ></ContactList>
+      <GlobalStyle />
+    </Container>
+  );
+};
